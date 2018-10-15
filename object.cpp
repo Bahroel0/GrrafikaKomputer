195458ce61e315d7e6b9852f65b4d42e44a6f2b6
Bahroel0 <bahrul123amaruddin@gmail.com>
2018-10-16 05:07:16 +0700

    file 0ff

--- object.cpp
+++ object.cpp
@@ -10,6 +10,10 @@ typedef struct {
 }point3D_t;
 
 typedef struct {
+    float x, y, z, r, g, b;
+}point3D_color;
+
+typedef struct {
     int numofVertices;
     short int pnt[32];
 }face_t;
@@ -20,6 +24,21 @@ typedef struct {
     int numofFaces;
     face_t fc[32];
 }object3D_t;
+
+typedef struct {
+    int numofVertices;
+    point3D_t pnt[100];
+    int numofFaces;
+    face_t fc[32];
+}object3D_t;
+
+typedef struct {
+    int numofVertices;
+    point3D_color pnt[100];
+    int numofFaces;
+    face_t fc[32];
+}object3D_color;
+
 
 void Polygon(point2D_t pnt[],int n, RGB color){
     glColor3f(color.r, color.g, color.b);
@@ -41,37 +60,39 @@ void init(){
 
 void Draw(){
     init();
-    object3D_t kubus = {
-        8,
-        {
-            {-25, -25, 25}, {25,-25,25}, {25,25,25}, {-25,25,25}, 
-            {-25,25,-25}, {25,25,-25}, {25,-25,-25}, {-25,-25,-25}
-        }, 
-        12,
-        {
-            {3,{3,2,1}}, {3,{0,3,1}}, {3,{0,1,7}}, {3,{4,5,6}}, 
-            {3,{4,6,7}}, {3,{0,3,7}}, {3,{1,2,6}}, {3,{1,6,7}},  
-            {3,{2,3,4}}, {3,{2,4,5}}, {3,{2,5,6}}, {3,{3,4,7}}   
-        }
-    };
-    printf("Banyaknya Titik : %d \n", kubus.numofVertices);
-    printf("Banyaknya Face  : %d \n", kubus.numofFaces);
-    point2D_t object[36];
-    int index = 0;
-    for(int i=0; i<kubus.numofFaces; i++){
-        printf("Face ke-%d, vertex : %d  ==>\n", i, kubus.fc[i].numofVertices );
-        for(int j=0; j<kubus.fc[i].numofVertices; j++){
-            int vertek = kubus.fc[i].pnt[j];
-            printf("   %d : %.0f, %.0f, %.0f", kubus.fc[i].pnt[j], kubus.pnt[vertek].x, kubus.pnt[vertek].y, kubus.pnt[vertek].z);
-            printf("\n");
 
-            // insert vertex
-            object[index].x = kubus.pnt[vertek].x;
-            object[index].y = kubus.pnt[vertek].y;
-            index++;
-        }
-    }
-    Polygon(object, 36, {1.0, 0.0, 0.0});
+    
+    // object3D_t kubus = {
+    //     8,
+    //     {
+    //         {-25, -25, 25}, {25,-25,25}, {25,25,25}, {-25,25,25}, 
+    //         {-25,25,-25}, {25,25,-25}, {25,-25,-25}, {-25,-25,-25}
+    //     }, 
+    //     12,
+    //     {
+    //         {3,{3,2,1}}, {3,{0,3,1}}, {3,{0,1,7}}, {3,{4,5,6}}, 
+    //         {3,{4,6,7}}, {3,{0,3,7}}, {3,{1,2,6}}, {3,{1,6,7}},  
+    //         {3,{2,3,4}}, {3,{2,4,5}}, {3,{2,5,6}}, {3,{3,4,7}}   
+    //     }
+    // };
+    // printf("Banyaknya Titik : %d \n", kubus.numofVertices);
+    // printf("Banyaknya Face  : %d \n", kubus.numofFaces);
+    // point2D_t object[36];
+    // int index = 0;
+    // for(int i=0; i<kubus.numofFaces; i++){
+    //     printf("Face ke-%d, vertex : %d  ==>\n", i, kubus.fc[i].numofVertices );
+    //     for(int j=0; j<kubus.fc[i].numofVertices; j++){
+    //         int vertek = kubus.fc[i].pnt[j];
+    //         printf("   %d : %.0f, %.0f, %.0f", kubus.fc[i].pnt[j], kubus.pnt[vertek].x, kubus.pnt[vertek].y, kubus.pnt[vertek].z);
+    //         printf("\n");
+
+    //         // insert vertex
+    //         object[index].x = kubus.pnt[vertek].x;
+    //         object[index].y = kubus.pnt[vertek].y;
+    //         index++;
+    //     }
+    // }
+    // Polygon(object, 36, {1.0, 0.0, 0.0});
     printf("\n\n");
     glFlush();
 }
