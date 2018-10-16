a6409c314d19ad6ac623f18568cfd696b6ac4a35
Bahroel0 <bahrul123amaruddin@gmail.com>
2018-10-16 11:16:28 +0700

    tabung not fixed

--- object.cpp
+++ object.cpp
@@ -21,23 +21,16 @@ typedef struct {
 
 typedef struct {
     int numofVertices;
-    point3D_t pnt[100];
+    point3D_t pnt[255];
     int numofFaces;
-    face_t fc[32];
+    face_t face[255];
 }object3D_t;
-
-// typedef struct {
-//     int numofVertices;
-//     point3D_t pnt[100];
-//     int numofFaces;
-//     face_t fc[32];
-// }object3D_t;
 
 typedef struct {
     int numofVertices;
     point3D_color pnt[100];
     int numofFaces;
-    face_t fc[32];
+    face_t face[32];
 }object3D_color;
 
 
@@ -61,9 +54,7 @@ void init(){
 
 int char_int(char *d) { int sum = atoi(d); return(int) sum; }
 
-void Draw(){
-    init();
-
+void FromFileOFF(){
     FILE *fp;
     char *line = NULL;
     size_t len = 0;
@@ -95,6 +86,7 @@ void Draw(){
         printf("Titik %d: %d %d %d, RGB: %d, %d, %d\n", l, row[l+2][0], row[l+2][1], row[l+2][2], row[l+2][3], row[l+2][4], row[l+2][5]);
     }
     
+    printf("\n");
     for(int m=0; m<row[1][1]; m++){
         int kolomFace = row[m+2+row[1][0]][0];
         int h=1;
@@ -108,38 +100,161 @@ void Draw(){
 
     free(line);
     exit(EXIT_SUCCESS);
-    
-    // object3D_t kubus = {
-    //     8,
-    //     {
-    //         {-25, -25, 25}, {25,-25,25}, {25,25,25}, {-25,25,25}, 
-    //         {-25,25,-25}, {25,25,-25}, {25,-25,-25}, {-25,-25,-25}
-    //     }, 
-    //     12,
-    //     {
-    //         {3,{3,2,1}}, {3,{0,3,1}}, {3,{0,1,7}}, {3,{4,5,6}}, 
-    //         {3,{4,6,7}}, {3,{0,3,7}}, {3,{1,2,6}}, {3,{1,6,7}},  
-    //         {3,{2,3,4}}, {3,{2,4,5}}, {3,{2,5,6}}, {3,{3,4,7}}   
-    //     }
-    // };
-    // printf("Banyaknya Titik : %d \n", kubus.numofVertices);
-    // printf("Banyaknya Face  : %d \n", kubus.numofFaces);
-    // point2D_t object[36];
-    // int index = 0;
-    // for(int i=0; i<kubus.numofFaces; i++){
-    //     printf("Face ke-%d, vertex : %d  ==>\n", i, kubus.fc[i].numofVertices );
-    //     for(int j=0; j<kubus.fc[i].numofVertices; j++){
-    //         int vertek = kubus.fc[i].pnt[j];
-    //         printf("   %d : %.0f, %.0f, %.0f", kubus.fc[i].pnt[j], kubus.pnt[vertek].x, kubus.pnt[vertek].y, kubus.pnt[vertek].z);
-    //         printf("\n");
+}
 
-    //         // insert vertex
-    //         object[index].x = kubus.pnt[vertek].x;
-    //         object[index].y = kubus.pnt[vertek].y;
-    //         index++;
-    //     }
-    // }
-    // Polygon(object, 36, {1.0, 0.0, 0.0});
+void ObjectKubus(){
+    object3D_t kubus = {
+        8,
+        {
+            {-25, -25, 25}, {25,-25,25}, {25,25,25}, {-25,25,25}, 
+            {-25,25,-25}, {25,25,-25}, {25,-25,-25}, {-25,-25,-25}
+        }, 
+        12,
+        {
+            {3,{3,2,1}}, {3,{0,3,1}}, {3,{0,1,7}}, {3,{4,5,6}}, 
+            {3,{4,6,7}}, {3,{0,3,7}}, {3,{1,2,6}}, {3,{1,6,7}},  
+            {3,{2,3,4}}, {3,{2,4,5}}, {3,{2,5,6}}, {3,{3,4,7}}   
+        }
+    };
+    printf("Banyaknya Titik : %d \n", kubus.numofVertices);
+    printf("Banyaknya Face  : %d \n", kubus.numofFaces);
+    point2D_t object[36];
+    int index = 0;
+    for(int i=0; i<kubus.numofFaces; i++){
+        printf("Face ke-%d, vertex : %d  ==>\n", i, kubus.face[i].numofVertices );
+        for(int j=0; j<kubus.face[i].numofVertices; j++){
+            int vertek = kubus.face[i].pnt[j];
+            printf("   %d : %.0f, %.0f, %.0f", kubus.face[i].pnt[j], kubus.pnt[vertek].x, kubus.pnt[vertek].y, kubus.pnt[vertek].z);
+            printf("\n");
+
+            // insert vertex
+            object[index].x = kubus.pnt[vertek].x;
+            object[index].y = kubus.pnt[vertek].y;
+            index++;
+        }
+    }
+    Polygon(object, 36, {1.0, 0.0, 0.0});
+}
+
+void GambarkanObject3D(int numofVertices, point3D_t *pnt,int numofFaces, face_t *face){
+    printf("Banyaknya Titik : %d \n", numofVertices);
+    printf("Banyaknya Face  : %d \n", numofFaces);
+
+    point2D_t points[numofVertices];
+    int t;
+    for(t=0; t<numofVertices; t++){
+        printf("Titik %d : %.0f, %.0f, %.0f \n", t, pnt[t].x, pnt[t].y, pnt[t].z);
+        points[t].x = pnt[t].x;
+        points[t].y = pnt[t].y;
+    }
+
+    for(int i=0; i < numofFaces; i++){
+        printf("Face ke-%d, vertex : %d  ==>\n", i, face[i].numofVertices );
+        for(int j=0; j < face[i].numofVertices; j++){
+            int vertek = face[i].pnt[j];
+            printf("   %d : %.0f, %.0f, %.0f", face[i].pnt[j], pnt[vertek].x, pnt[vertek].y, pnt[vertek].z);
+            printf("\n");
+        }
+    }
+    Polygon(points, numofVertices, {1.0, 0.0, 0.0});
+}
+
+void Tabung(){
+    
+    int n = 12;
+    int pointCount = 2*n+1;
+    int faceCount = 3*n;
+    int pointCenterTop = 0;
+    int rangeTop = 75;
+    int pointCenterBottom = 0;
+    int rangeBottom = -75;
+    point3D_t point[pointCount];
+    face_t face[36];
+
+    // inisialisasi titik
+    int i, r = 100, teta =0;
+    for(i=0; i<pointCount; i++){
+        if(i<n){
+            point[i].x = r * cos(teta);
+            point[i].y = rangeTop;
+            point[i].z = r * sin(teta);
+        }else{
+            point[i].x = r * cos(teta);
+            point[i].y = rangeBottom;
+            point[i].z = r * sin(teta);
+        }
+        if(teta <330){
+            teta += 360/n;
+        }else{
+            teta = 0;
+        }
+    }
+
+    // inisialisasi face
+    int j, pointTop3 = 0, pointBottom3 = n, pointBottom4 = n, pointTop4 = 0;
+    bool first = true;
+    for(j=0; j<faceCount; j++){
+        if(j<(n-1)){
+            face[j].numofVertices = 3;
+            // inisialisasi titik pada face
+            int k;
+            first = true;
+            for(k=0; k < face[j].numofVertices; k++){
+                if(first){
+                    face[j].pnt[k] = pointCenterTop;
+                    first = false; 
+                }else{
+                    face[j].pnt[k] = pointTop3; 
+                }
+                pointTop3++;
+            }
+            pointTop3 = pointTop3-2;
+        }else if(j == (n-1)){
+            face[j].pnt[0] = pointCenterTop;
+            face[j].pnt[1] = pointCenterTop+1;
+            face[j].pnt[2] = n-1;
+        }else if(j>(n-1) && j<(2*n-1)){
+            face[j].numofVertices = 3;
+            // inisialisasi titik pada face
+            int l;
+            first = true;
+            for(l=0; l < face[j].numofVertices; l++){
+                if(first){
+                    face[j].pnt[l] = pointBottom3;
+                }else{
+                    face[j].pnt[l] = pointBottom3; 
+                }
+                pointBottom3++;
+            }
+            pointBottom3 = pointBottom3 - 2;
+        }else if(j == (faceCount-1)){
+            face[j].numofVertices = 4;
+            // inisialisasi titik pada face
+            face[j].pnt[0] = pointTop4;
+            face[j].pnt[1] = pointBottom4;
+            face[j].pnt[2] = pointCenterTop + 1;
+            face[j].pnt[3] = pointCenterBottom + 1 + n;
+        }else{
+            face[j].numofVertices = 4;
+            // inisialisasi titik pada face
+            face[j].pnt[0] = pointTop4;
+            face[j].pnt[1] = pointBottom4;
+            face[j].pnt[2] = pointTop4 + 1;
+            face[j].pnt[3] = pointBottom4 + 1;
+            pointBottom4++;
+            pointTop4++;           
+        }
+    }
+
+    GambarkanObject3D(pointCount, point, faceCount, face);
+}
+
+void Draw(){
+    init();
+
+    // FromFileOFF();
+
+    Tabung();
     printf("\n\n");
     glFlush();
 }
@@ -151,7 +266,7 @@ int main(int Argc, char* argv[]){
     glutInit(&Argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(180, 180);
+	glutInitWindowPosition(400, 400);
 	glutCreateWindow("Grafika Komputer | 2103161005 | Bahrul Amaruddin");
 	glutIdleFunc(Draw);
 	glutDisplayFunc(Draw);
---
+++ silinder.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <math.h>
+
+#define PI 3.1415927
+
+void draw_cylinder(GLfloat radius,
+                   GLfloat height,
+                   GLubyte R,
+                   GLubyte G,
+                   GLubyte B)
+{
+    GLfloat x              = 0.0;
+    GLfloat y              = 0.0;
+    GLfloat angle          = 0.0;
+    GLfloat angle_stepsize = 0.1;
+
+    /** Draw the tube */
+    glColor3ub(R-40,G-40,B-40);
+    glBegin(GL_QUAD_STRIP);
+    angle = 0.0;
+        while( angle < 2*PI ) {
+            x = radius * cos(angle);
+            y = radius * sin(angle);
+            glVertex3f(x, y , height);
+            glVertex3f(x, y , 0.0);
+            angle = angle + angle_stepsize;
+        }
+        glVertex3f(radius, 0.0, height);
+        glVertex3f(radius, 0.0, 0.0);
+    glEnd();
+
+    /** Draw the circle on top of cylinder */
+    glColor3ub(R,G,B);
+    glBegin(GL_POLYGON);
+    angle = 0.0;
+        while( angle < 2*PI ) {
+            x = radius * cos(angle);
+            y = radius * sin(angle);
+            glVertex3f(x, y , height);
+            angle = angle + angle_stepsize;
+        }
+        glVertex3f(radius, 0.0, height);
+    glEnd();
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    glTranslatef(0.0,-0.4,-3.0);
+    glRotatef(-40, 1.0, 0.0, 0.0);
+
+    draw_cylinder(0.3, 1.0, 255, 160, 100);
+
+    glFlush();
+}
+
+void reshape(int width, int height)
+{
+    if (width == 0 || height == 0) return;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(40.0, (GLdouble)width/(GLdouble)height,
+                   0.5, 20.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 0, width, height);
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640,480);
+    glutCreateWindow("Create Cylinder");
+    glClearColor(0.0,0.0,0.0,0.0);
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
+
+    return 0;
+}
